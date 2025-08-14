1f590e3636d71929c6bd645bb94362b5771ab280
BiraEusova <ira.breusova@gmail.com>
2025-08-14 15:57:31 +0700

    feat: add ReactPlayer
    change video tag to implementation of video player in library ReactPlayer

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,21 +1,25 @@
-import style from './VideoPlayer.module.css'
+import style from './VideoPlayer.module.css';
+import ReactPlayer from 'react-player';
 
 const VideoPlayer = () => {
 
 	return (
-		<video
-			className={style.videoPlayer}
-			autoPlay
+		<ReactPlayer
+			playing={true}
 			muted
 			preload
-			loop
+			loop={true}
+			style={{
+				width: '100%',
+				height: 'auto'
+			}}
 		>
 			<source src="src/assets/video/mountain-fog-video.webm" type="video/webm"/>
 			<source src="src/assets/video/mountain-fog-video.mp4" type="video/mp4"/>
 			<p>Что-то пошло не так :( Попробуйте скачать видео по
 				<a download href="../assets/video/mountain-fog-video.mp4">ссылке</a>
 			</p>
-		</video>
+		</ReactPlayer>
 	)
 }
 
